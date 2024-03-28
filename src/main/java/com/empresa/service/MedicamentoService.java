51485762848abb9fe6c25770043b4f20f6be8e7f
AngeloHilario <angelohilariolipe@gmail.com>
2024-03-27 20:31:16 -0500

    tarea realizada

---
+++ src/main/java/com/empresa/service/MedicamentoService.java
@@ -0,0 +1,14 @@
+package com.empresa.service;
+
+import java.util.List;
+
+import com.empresa.entity.Medicamento;
+
+public interface MedicamentoService {
+
+	public abstract List<Medicamento> listaMedicamento();
+	public abstract List<Medicamento> listaMedicamentoPorLaboratorio(String laboratorio);
+	public abstract List<Medicamento> listaMedicamentoPorNombreLike(String nombre);
+	public abstract List<Medicamento> listaMedicamentoPorId(int idMedicamento);
+	
+}
